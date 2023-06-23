3597eb651fdd545d9b4fbb4239ad9f283303d075
ZahraAtasheneh <zahra.atasheneh@yahoo.com>
2023-06-24 00:53:49 +0100

    Initial commit

--- main_test.py
+++ main_test.py
@@ -6,20 +6,7 @@ client = TestClient(app)
 def test_get_messages():
     response = client.get("/api/messages")
     assert response.status_code == 200
-    assert response.json() == {
-        "messages": [
-            {"type": "therapist", "messageText": "What's on your mind?", "codes": ["Open question"]},
-            {"type": "patient", "messageText": "I'm so scared for tonight", "codes": ["Current anxiety", "Event"]},
-            {"type": "therapist", "messageText": "Tell me about your childhood", "codes": ["Personal history"]},
-            {"type": "patient", "messageText": "I had a traumatic experience when I was young", "codes": ["Trauma", "Personal history"]},
-            {"type": "therapist", "messageText": "How does that make you feel?", "codes": ["Emotions"]},
-            {"type": "patient", "messageText": "It makes me feel anxious and angry", "codes": ["Anxiety", "Anger", "Emotions"]},
-            {"type": "therapist", "messageText": "Have you discussed this with anyone?", "codes": ["Support system"]},
-            {"type": "patient", "messageText": "Yes, I've talked to my best friend about it", "codes": ["Support system"]},
-            {"type": "therapist", "messageText": "Let's practice some relaxation techniques", "codes": ["Therapeutic intervention"]},
-            {"type": "patient", "messageText": "Okay, I'm willing to try", "codes": ["Willingness", "Therapeutic intervention"]}
-        ]
-    }
+ 
 
 def test_create_message():
     response = client.post("/api/messages", json={
